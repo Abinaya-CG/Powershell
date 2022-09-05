f3e0b3e103a869d4e3b0cf86bae2c28e156e881c
Abinaya-CG <109946078+Abinaya-CG@users.noreply.github.com>
2022-09-05 16:17:44 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -3,4 +3,6 @@ $user = "Administrator"
 $pass= "-IYQINoaR*L3KW$cg4t!GWaECAW9ukXu"
 $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
-Invoke-command -ScriptBlock {Get-Date} -ComputerName 44.204.6.171 -credential $cred
+$session = new-pssession 44.204.6.171 -credential $credential  
+enter-pssession $session  
+Write-Host "Hello, World (from $env:COMPUTERNAME)"
