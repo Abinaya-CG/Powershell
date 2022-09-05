2f38e7bf603d593112af701cb10fd460883846cd
Abinaya-CG <109946078+Abinaya-CG@users.noreply.github.com>
2022-09-05 15:09:10 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,2 +1,6 @@
 /usr/bin/pwsh
-echo "Hello World!!"
+$user = "Administrator"
+$pass= "-IYQINoaR*L3KW$cg4t!GWaECAW9ukXu"
+$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
+console.log (Invoke-Command -ComputerName 44.204.6.171 -ScriptBlock { Get-ChildItem C:\ } -credential $cred)
