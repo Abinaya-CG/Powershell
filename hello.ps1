22053102430b6e3a63f591519f22fe335e8f5ad4
Abinaya-CG <109946078+Abinaya-CG@users.noreply.github.com>
2022-09-05 15:20:55 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -3,4 +3,4 @@ $user = "Administrator"
 $pass= "-IYQINoaR*L3KW$cg4t!GWaECAW9ukXu"
 $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
-console.log (Invoke-Command -ComputerName 44.204.6.171 -ScriptBlock { Get-ChildItem C:\ } -credential $cred)
+console.log (Invoke-Command -ComputerName 44.204.6.171 -ScriptBlock {Get-Date;} -credential $cred)
