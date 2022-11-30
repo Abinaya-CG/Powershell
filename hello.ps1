12d4a1e369658c6a3a27a1e2a8f4faf2350b4f5a
Abinaya-CG <109946078+Abinaya-CG@users.noreply.github.com>
2022-11-30 16:47:47 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -5,4 +5,4 @@ $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
 $session = new-pssession ec2-35-89-57-61.us-west-2.compute.amazonaws.com -credential $cred 
 enter-pssession $session  
-Write-Host "Hello, World..sdsd....(from $env:COMPUTERNAME)"
+Write-Host "Hello, World.....(from $env:COMPUTERNAME)"
